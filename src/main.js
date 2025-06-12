493d4a691ea9b8b4b596c00282d21154ff4f8d38
Garison Zagorski <garisonzag88@gmail.com>
2025-06-12 17:42:07 -0400

    Initial game setup
    Created the window and character

---
+++ src/main.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const canvas = document.querySelector('.window');
+    const character = document.querySelector('.main_character');
+    
+    
+});
